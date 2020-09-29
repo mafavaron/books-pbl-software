19a692bd3915a83c1b69e072525159d74fa1a857
Patrizia M. Favaron <mafavaron@mac.com>
2020-09-29 23:35:24 +0200

    Update plume_rise.jl
    Stack tip wind speed estimation by the ISC3 power law

--- plume_rise/plume_rise.jl
+++ plume_rise/plume_rise.jl
@@ -1,3 +1,37 @@
+# Stima della velocità del vento alla quota geometrica
+# della ciminiera
+function us(
+    zr,    # Quota alla quale è misurato il vento (m)
+    ur,    # Velocità del vento misurata alla quota 'zr' (m/s)
+    hs,    # Altezza geometrica della ciminiera (m)
+    istab, # Categoria di stabilità (1=A, 2=B, ..., 6=F)
+    env_id # Identificatore di tipo ambiente (1=rurale, 2=urbano)
+)
+
+    # Basic parameter check
+    if istab < 1 or istab > 6 or env_id < 1 or env_id > 2
+        return missing
+    end
+
+    # Determina l'esponente in base alla stabilità
+    # ed al tipo di ambiente
+    p_rural = [0.07, 0.07, 0.10, 0.15, 0.35, 0.55]
+    p_urban = [0.15, 0.15, 0.20, 0.25, 0.30, 0.30]
+    if env_id == 1 # Rurale
+        p = p_rural[istab]
+    elseif env_id == 2 # Urbano
+        p = p_urban[istab]
+    else
+        return missing
+    end
+
+    # Il calcolo vero e proprio
+    us_value = ur*(hs/zr)^p
+    return us_value
+
+end
+
+
 # Abbassamento del pennacchio per effetto di scia
 # al vertice della ciminiera.
 function h_prime(
