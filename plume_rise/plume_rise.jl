b26d083823c6d9b8d87c4fa123c8804debf7a716
Patrizia M. Favaron <mafavaron@mac.com>
2020-09-29 23:19:04 +0200

    Update plume_rise.jl
    Stack-tip downwash added

--- plume_rise/plume_rise.jl
+++ plume_rise/plume_rise.jl
@@ -1,3 +1,20 @@
+# Abbassamento del pennacchio per effetto di scia
+# al vertice della ciminiera.
+function h_prime(
+    hs, # Altezza geometrica della ciminiera (m)
+    vs, # Velocità verticale dei fumi all'uscita dalla ciminiera (m/s)
+    ds, # Diametro della ciminiera al vertice (m)
+    us  # Velocità del vento alla quota geometrica della ciminiera (m/s)
+)
+    if vs < 1.5*us
+        hs_p = hs + 2*ds*(vs/us - 1.5)
+    else
+        hs_p = hs
+    end
+    return hs
+end
+
+
 function Fb(vs,ds,Ts,Ta)
     const g = 9.807
     return g * vs * ds^2 * (Ts-Ta)/(4.*Ts)
@@ -10,5 +27,5 @@ end
 
 
 function max_z(istab, us, vs, ds, ts)
-    
+
 end
