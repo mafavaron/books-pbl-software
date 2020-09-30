83a24ec7ed24fb8c8c45b64a16799280455d79ac
Patrizia M. Favaron <mafavaron@mac.com>
2020-09-30 19:34:13 +0200

    Update plume_rise.jl
    In progress

--- plume_rise/plume_rise.jl
+++ plume_rise/plume_rise.jl
@@ -49,31 +49,69 @@ function h_prime(
 end
 
 
-function Fb(vs,ds,Ts,Ta)
+# Flusso di galleggiamento
+function Fb(
+    vs, # Velocità verticale dei fumi all'uscita dalla ciminiera (m/s)
+    ds, # Diametro della ciminiera al vertice (m)
+    Ts, # Temperatura dei fumi all'uscita dalla ciminiera (K)
+    Ta  # Temperatura ambiente (K)
+)
     const g = 9.807
     return g * vs * ds^2 * (Ts-Ta)/(4.*Ts)
 end
 
 
-function Fm(vs,ds,Ts,Ta)
+function Fm(
+    vs, # Velocità verticale dei fumi all'uscita dalla ciminiera (m/s)
+    ds, # Diametro della ciminiera al vertice (m)
+    Ts, # Temperatura dei fumi all'uscita dalla ciminiera (K)
+    Ta  # Temperatura ambiente (K)
+)
     return vs^2 * ds^2 * Ta/(4.*Ts)
 end
 
 
-function max_z(istab, hs, us, vs, ds, Ts, Ta)
+function max_z(
+    istab, # Categoria di stabilità (1=A, 2=B, ..., 6=F)
+    hs, # Altezza geometrica della ciminiera (m)
+    us, # Velocità del vento alla quota del vertice della ciminiera (m/s)
+    vs, # Velocità verticale dei fumi all'uscita dalla ciminiera (m/s)
+    ds, # Diametro della ciminiera al vertice (m)
+    Ts, # Temperatura dei fumi all'uscita dalla ciminiera (K)
+    Ta  # Temperatura ambiente (K)
+)
 
     # Stable, or neutral/convective?
-    if istab <= 3
+    if istab <= 4
         stable = true
     else
         stable = false
     end
 
+    # Calcolo dei flussi di galleggiamento e
+    # di quantità di moto
+    fb = Fb(vs,ds,Ts,Ta)
+    fm = Fm(vs,ds,Ts,Ta)
+
     # Stima della distanza sottovento alla quale
     # il pennacchio raggiunge la sua quota massima
     if stable
-        xb = 2.0715*us/sqrt(s)
+        if istab == 5
+            s = 0.020
+        else
+            s = 0.035
+        end
+        xb = 2.0715 * us/sqrt(s)
+        xm = 3.1415927/2. * us/sqrt(s)
     else
+        if fb > 55
+            xb = 119.0 * fb^(2./5.)
+        elseif fb > 0.
+            xb = 49.0 * fb^(5./8.)
+        else
+            xb = 4.0 * ds * (vs+3*ds)^2/(vs*ds)
+        end
+        xm = 4.0 * ds * (vs+3*ds)^2/(vs*ds)
     end
 
 end
