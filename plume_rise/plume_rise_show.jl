bfba392cacec25a1c9338afbe31f9e17069d43b7
Patrizia M. Favaron <mafavaron@mac.com>
2020-10-02 06:38:43 +0200

    Update plume_rise_show.jl
    In progress

--- plume_rise/plume_rise_show.jl
+++ plume_rise/plume_rise_show.jl
@@ -4,12 +4,13 @@
 #
 # Questo codice è coperto dalla licenza MIT.
 
-function show_xmax(
+function show_max_x_vs(
     istab::Int32,           # Categoria di stabilità (1=A, 2=B, ..., 6=F)
     us::Float64,            # Velocità del vento alla quota del vertice della ciminiera (m/s)
-    vs::Array{Float64,1},   # Velocità verticale dei fumi all'uscita dalla ciminiera (m/s)
+    vs_min::Float64,        # Velocità verticale minima dei fumi all'uscita dalla ciminiera (m/s)
+    vs_max::Float64,        # Velocità verticale massima dei fumi all'uscita dalla ciminiera (m/s)
     ds::Float64,            # Diametro della ciminiera al vertice (m)
-    Ts::Array{Float64,1},   # Temperatura dei fumi all'uscita dalla ciminiera (K)
+    Ts::Float64,            # Temperatura dei fumi all'uscita dalla ciminiera (K)
     Ta::Float64             # Temperatura ambiente (K)
 )
 
@@ -19,5 +20,5 @@ function show_xmax(
         error "show_xmax:: error: Lengths of 'vs' and 'Ts' should be equal"
     end
 
-    
+
 end
