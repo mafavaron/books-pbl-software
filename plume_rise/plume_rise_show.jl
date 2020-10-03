a8afc5f060c9912137cf25115dea75ff93324a15
Patrizia M. Favaron <mafavaron@mac.com>
2020-10-03 17:41:19 +0200

    Update plume_rise_show.jl
    In progress

--- plume_rise/plume_rise_show.jl
+++ plume_rise/plume_rise_show.jl
@@ -7,7 +7,7 @@
 using Plotly
 
 function show_max_x_vs(
-    istab::Int32,           # Categoria di stabilità (1=A, 2=B, ..., 6=F)
+    istab::Int64,           # Categoria di stabilità (1=A, 2=B, ..., 6=F)
     us::Float64,            # Velocità del vento alla quota del vertice della ciminiera (m/s)
     vs_min::Float64,        # Velocità verticale minima dei fumi all'uscita dalla ciminiera (m/s)
     vs_max::Float64,        # Velocità verticale massima dei fumi all'uscita dalla ciminiera (m/s)
@@ -22,5 +22,10 @@ function show_max_x_vs(
     for i in 1:length(vs)
         max_x_vals[i] = max_x(istab, us, vs_val, ds, Ts, Ta)
     end
+
+    trace = ["x" => vs, "y" => max_x_vals, "type" => "scatter"]
+    data = [trace]
+    response = Plotly.plot(data, ["filename" => "basic-line", "fileopt" => "overwrite"])
+    plot_url = response["url"]
 
 end
