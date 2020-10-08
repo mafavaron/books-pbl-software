39d4945320a8a002518d56917ffc3b1c2fd64af2
Patrizia M. Favaron <mafavaron@mac.com>
2020-10-08 07:08:26 +0200

    Update plume_rise_show.jl
    Correction, to see actually the plot

--- plume_rise/plume_rise_show.jl
+++ plume_rise/plume_rise_show.jl
@@ -7,7 +7,6 @@
 using Plots
 
 function show_max_x_vs(
-    istab::Int64,           # Categoria di stabilità (1=A, 2=B, ..., 6=F)
     us::Float64,            # Velocità del vento alla quota del vertice della ciminiera (m/s)
     vs_min::Float64,        # Velocità verticale minima dei fumi all'uscita dalla ciminiera (m/s)
     vs_max::Float64,        # Velocità verticale massima dei fumi all'uscita dalla ciminiera (m/s)
@@ -16,14 +15,25 @@ function show_max_x_vs(
     Ta::Float64             # Temperatura ambiente (K)
 )
 
+    gr()
+
     vs = range(vs_min, vs_max; length=100)
 
-    max_x_vals = Vector{Float64}(undef, length(vs))
+    max_x_vals = Array{Float64}(undef, length(vs), 2)
     for i in 1:length(vs)
-        max_x_vals[i] = max_x(istab, us, vs[i], ds, Ts, Ta)
+        max_x_vals[i,1] = max_x(1, us, vs[i], ds, Ts, Ta) # Categoria A: convettività
+        max_x_vals[i,2] = max_x(6, us, vs[i], ds, Ts, Ta) # Categoria F: stabilità
     end
 
-    gr()    # Force graphic backend to GR
-    plot(vs, max_x_vals, xlabel="Vel. fumi (m/s)", ylabel="x Max h (m)")
+    p = plot(vs, max_x_vals, label = ["Cat. A" "Cat. F"]);
+    xlabel!("Vel. fumi (m/s)");
+    ylabel!("x alla max altezza (m)");
+    display(p)
+
+    println(max_x_vals[1,1])
+    println(max_x_vals[2,2])
+
+    println(max_x_vals[length(vs),1])
+    println(max_x_vals[length(vs),2])
 
 end
